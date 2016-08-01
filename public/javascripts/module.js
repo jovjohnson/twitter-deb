30d83936244c06f6d430b1aa40898c5ac64900d7
Jovvi Johnson <jjohnsow@gmail.com>
2016-07-31 20:19:15 -0700

    colors

--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var app = angular.module('debateApp', ['chart.js']);
+
+
+app.config(function (ChartJsProvider) {
+  console.log('config');
+  ChartJsProvider.setOptions({ colors : [ '#80deea', '#80deea', '#80deea', '#80deea', '#80deea', '#80deea', '#80deea'] });
+});
