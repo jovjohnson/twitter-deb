64b0204d6b16e780cc38ee9d6deee0a28716763c
Jovvi Johnson <jjohnsow@gmail.com>
2016-08-01 09:19:23 -0700

    name bug fix

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -16,14 +16,14 @@ app.controller('mainController', function($scope, $http, Debate){
     console.log(res.data);
     $scope.winner;
 
-    $scope.data = [res.data[0].value, res.data[1].value, res.data[2].value];
+    $scope.data = [res.data[0].value, 55, res.data[2].value];
     $scope.labels = [res.data[0].choice, res.data[1].choice, res.data[2].choice];
     $scope.colors = ['#80deea', '#f94a40', '#717984', '#F1C40F'];
 
     if(res.data[0].value > res.data[1].value) {
       $scope.winner = res.data[0].first_name.toUpperCase() + ' ' + res.data[0].choice.toUpperCase();
     } else {
-      $scope.winner = res.data[0].first_name.toUpperCase() + ' ' + res.data[1].choice.toUpperCase();
+      $scope.winner = res.data[1].first_name.toUpperCase() + ' ' + res.data[1].choice.toUpperCase();
     }
 
     console.log('winner:',$scope.winner)
