1d08f2fc8b2e4a6885a6edb4e939f28e46cc737b
Jovvi Johnson <jjohnsow@gmail.com>
2016-08-05 19:37:15 -0700

    css

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -16,6 +16,10 @@ app.controller('mainController', function($scope, $http, Debate){
     $scope.labels = [res.data[0].choice, res.data[1].choice, res.data[2].choice];
     $scope.colors = ['#80deea', '#f94a40', '#f9e504', '#F1C40F'];
 
+    $scope.clintonNum = res.data[0].value;
+    $scope.trumpNum = res.data[1].value;
+    $scope.johnsonNum = res.data[2].value;
+
     if(res.data[0].value > res.data[1].value) {
       $scope.winner = res.data[0].first_name.toUpperCase() + ' ' + res.data[0].choice.toUpperCase();
     } else {
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -5,7 +5,6 @@ var app = angular.module('debateApp');
 app.service('Debate', function($http){
 
   this.get = function() {
-    // return $http.get('/tweets');
     return $http.get('/poll-data');
   }
 
